33b0205489454e5127b6592102d3fdd4f9d26bdc
Jonatas Amorim Silva <jonatas231997@live.com>
2022-09-19 22:35:54 -0300

    Scructuring the database with sqlite and existing user validation

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,17 @@
 const AppError = require("../utils/AppError");
 
+const sqliteConnections = require("../database/sqlite");
+
 class UsersController {
   async create(request, response){
     const { name, email, password } = request.body;
 
-    if(!name){
-      throw new AppError("O campo nome e obrigatório!")
-    }
+    const database = await sqliteConnections();
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
+        if (checkUserExists) {
+            throw new AppError("Este e-mail já está em uso.");
+        }
     return response.status(201).json();
   }
 }
